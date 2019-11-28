2b277e3fa7e2e1ec0bdffb7aac39da3257974a9d
Novicor <m.dubovikas@gmail.com>
2019-11-28 10:40:41 +0200

    Balance fix

--- js/function.js
+++ js/function.js
@@ -1,22 +1,18 @@
-function renderBalance ( account ) {
-    const DOM = document.querySelector('balance');
+function renderBalance( account ) {
+    const DOM = document.querySelector('#balance');
     let HTML = '';
-
-    for ( let i=0; i<months.length; i++) {
+    for (let i = 0; i < months.length; i++) {
         const month = months[i];
-    
-        for ( let t=0; t<account.length; t++) {
-            const list = account[i]
-        
-
-    
-    HTML += `<div class="table-row">
+        for (let t = 0; t < account.length; t++) {
+            const list = account[i];
+            HTML += `<div class="table-row">
                 <div class="cell">${list.month}</div>
                 <div class="cell">${month[i]}</div>
                 <div class="cell">${list.income}</div>
                 <div class="cell">${list.expense}</div>
                 <div class="cell">${list.income} - ${list.expense}</div>
-            </div>`
+                </div>`;
+
         }
     }
     return DOM.innerHTML = HTML;
