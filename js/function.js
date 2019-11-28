e8d3d845372b15ec721c75d1ed2fb202676b9b66
Novicor <m.dubovikas@gmail.com>
2019-11-28 10:48:00 +0200

    balance #5

--- js/function.js
+++ js/function.js
@@ -5,14 +5,28 @@ function renderBalance( account ) {
         const month = months[i];
         for (let t = 0; t < account.length; t++) {
             const list = account[i];
+            for ( let k=0; k<list.income.length; k++) {
+                const totalIncome = list.income[i];
+                for ( let s=0; s<list.expense; s++) {
+                const totalExpense = list.expense[i];
+                const totalBalance = totalIncome - totalExpense;
+
             HTML += `<div class="table-row">
                 <div class="cell">${list.month}</div>
                 <div class="cell">${month[i]}</div>
                 <div class="cell">${list.income}</div>
                 <div class="cell">${list.expense}</div>
                 <div class="cell">${list.income} - ${list.expense}</div>
-                </div>`;
-
+                </div>
+                <div class="table-footer">
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell">${totalIncome}</div>
+                    <div class="cell">${totalExpense}</div>
+                    <div class="cell">${totalBalance}</div>
+                </div>`
+                }
+            }
         }
     }
     return DOM.innerHTML = HTML;
